a3b4335f42e0ccfaefde314da6406c9fbdc76461
mohamedelhefenawy <melhefnawy718@gmail.com>
2024-12-12 14:03:06 +0200

    delete price

--- src/components/Conference/Conference.jsx
+++ src/components/Conference/Conference.jsx
@@ -9,37 +9,31 @@ function Conference() {
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4>30 ر.س</h4>
   <button>احجز الان</button>
 </div>
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4> 30 ر.س </h4>
   <button>احجز الان</button>
 </div>
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4>30 ر.س</h4>
   <button>احجز الان</button>
 </div>
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4>30 ر.س</h4>
   <button>احجز الان</button>
 </div>
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4>30 ر.س</h4>
   <button>احجز الان</button>
 </div>
 <div className='con'>
   <img src={photo} alt="" />
   <h4>مؤتمر الجمعية</h4>
-  <h4>30 ر.س</h4>
   <button>احجز الان</button>
 </div>
 
