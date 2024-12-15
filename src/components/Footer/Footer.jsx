1952b68683c3f2f007f8d18aed3f446a30fb1481
engmohesham <mohamedhesham695@gmail.com>
2024-12-16 00:42:31 +0200

    ssd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import photo from "../../assets/images/logo.png";
 
 function Footer({ line, useline }) {
   return (
-    <div className="max-w-[80%] mx-auto mt-24 flex flex-col items-center gap-12">
+    <div className="w-[80%] mx-auto mt-24 flex flex-col items-center gap-5">
       <footer
         style={{
           // backgroundColor: "#F5F5F5",
@@ -13,84 +13,70 @@ function Footer({ line, useline }) {
           fontFamily: "'IBM Plex Sans Arabic', sans-serif",
           padding: "6px 0",
         }}
+        className="w-full"
       >
-        <div
-          style={{
-            maxWidth: "1000px",
-            margin: "auto",
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ flex: 1, textAlign: "center" }}>
+        <div className="w-full grid grid-cols-1 lg:grid-cols-4 gap-10 text-center lg:text-right">
+          <div className="text-center">
             <img
               src={photo}
               alt="King Fahad National Library"
-              style={{ maxWidth: "150px", marginBottom: "15px" }}
+              style={{
+                maxWidth: "150px",
+                marginBottom: "15px",
+                margin: "auto",
+              }}
             />
             <p>مكتبة الملك فهد الوطنية</p>
           </div>
 
-          <div
-            style={{
-              flex: 2,
-              display: "flex",
-              justifyContent: "space-around",
-              flexWrap: "wrap",
-              textAlign: "right",
-            }}
-          >
-            <div>
-              <a href="#" className="footer-link">
-                خريطة الموقع
-              </a>
-              <a href="#" className="footer-link">
-                أسئلة شائعة
-              </a>
-              <a href="#" className="footer-link">
-                سياسة الخصوصية
-              </a>
-              <a href="#" className="footer-link">
-                إتفاقية الاستخدام
-              </a>
-              <a href="#" className="footer-link">
-                التبليغ عن حادثة أمن معلومات
-              </a>
-              <a href="#" className="footer-link">
-                English
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://kfnl.gov.sa/Ar/About/ContactUs/Pages/ContactUs2.aspx"
-                className="footer-link"
-              >
-                اتصل بنا
-              </a>
-              <a href="#" className="footer-link">
-                قواعد المعلومات
-              </a>
-              <a href="#" className="footer-link">
-                جولة عامة في المكتبة
-              </a>
-              <a href="#" className="footer-link">
-                البريد الإلكتروني
-              </a>
-              <a href="#" className="footer-link">
-                إتفاقية مستوى الخدمة
-              </a>
-              <a href="#" className="footer-link">
-                مكتب تحقيق الرؤية
-              </a>
-            </div>
-            <div>
-              <p style={{ margin: "5px 0" }}>المملكة العربية السعودية</p>
-              <p style={{ margin: "5px 0" }}>ص.ب 7572 الرياض 11472</p>
-              <p style={{ margin: "5px 0" }}>هاتف: 0114861111</p>
-              <p style={{ margin: "5px 0" }}>فاكس: 0114862222</p>
-            </div>
+          <div className="flex flex-col gap-4">
+            <a href="#" className="footer-link text-nowrap">
+              خريطة الموقع
+            </a>
+            <a href="#" className="footer-link text-nowrap">
+              أسئلة شائعة
+            </a>
+            <a href="#" className="footer-link text-nowrap">
+              سياسة الخصوصية
+            </a>
+            <a href="#" className="footer-link text-nowrap">
+              إتفاقية الاستخدام
+            </a>
+            <a href="#" className="footer-link text-nowrap">
+              التبليغ عن حادثة أمن معلومات
+            </a>
+            <a href="#" className="footer-link text-nowrap">
+              English
+            </a>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a
+              href="https://kfnl.gov.sa/Ar/About/ContactUs/Pages/ContactUs2.aspx"
+              className="footer-link"
+            >
+              اتصل بنا
+            </a>
+            <a href="#" className="footer-link">
+              قواعد المعلومات
+            </a>
+            <a href="#" className="footer-link">
+              جولة عامة في المكتبة
+            </a>
+            <a href="#" className="footer-link">
+              البريد الإلكتروني
+            </a>
+            <a href="#" className="footer-link">
+              إتفاقية مستوى الخدمة
+            </a>
+            <a href="#" className="footer-link">
+              مكتب تحقيق الرؤية
+            </a>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p style={{ margin: "5px 0" }}>المملكة العربية السعودية</p>
+            <p style={{ margin: "5px 0" }}>ص.ب 7572 الرياض 11472</p>
+            <p style={{ margin: "5px 0" }}>هاتف: 0114861111</p>
+            <p style={{ margin: "5px 0" }}>فاكس: 0114862222</p>
           </div>
         </div>
 
